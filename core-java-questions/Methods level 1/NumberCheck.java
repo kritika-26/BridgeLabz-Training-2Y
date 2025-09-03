ddea2e61280fea1397a5f10a98a28124607e8b67
kritika-26 <kritika.saxena_cs.aiml24@gla.ac.in>
2025-09-03 12:31:33 +0530

    [Kritika] Added a new code of Cheking the number whether positive,negative or zero

---
+++ core-java-questions/Methods level 1/NumberCheck.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+
+    public static int checkNumber(int num) {
+        if (num > 0) {
+            return 1;   // positive
+        } else if (num < 0) {
+            return -1;  // negative
+        } else {
+            return 0;   // zero
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take input
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int result = checkNumber(number);
+
+        if (result == 1) {
+            System.out.println("The number is Positive.");
+        } else if (result == -1) {
+            System.out.println("The number is Negative.");
+        } else {
+            System.out.println("The number is Zero.");
+        }
+
+        sc.close();
+    }
+}
