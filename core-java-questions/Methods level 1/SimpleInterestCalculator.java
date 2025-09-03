422166953a9bb04e4bca155a7af22f6e2fa00213
kritika-26 <kritika.saxena_cs.aiml24@gla.ac.in>
2025-09-03 12:01:24 +0530

    [Kritika] Added Simple interest question

---
+++ core-java-questions/Methods level 1/SimpleInterestCalculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SimpleInterestCalculator {
+
+    // Method to calculate simple interest
+    public static double calculateSimpleInterest(double principal, double rate, double time) {
+        return (principal * rate * time) / 100;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input from user
+        System.out.print("Enter Principal amount: ");
+        double principal = sc.nextDouble();
+
+        System.out.print("Enter Rate of Interest: ");
+        double rate = sc.nextDouble();
+
+        System.out.print("Enter Time (in years): ");
+        double time = sc.nextDouble();
+
+        // Calling the method
+        double simpleInterest = calculateSimpleInterest(principal, rate, time);
+
+        // Displaying result
+        System.out.println("Simple Interest = " + simpleInterest);
+
+        sc.close();
+    }
+}
