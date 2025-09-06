bc2f27f83c1c8256c7dcd25da2395f591d3695a3
kritika-26 <kritika.saxena_cs.aiml24@gla.ac.in>
2025-09-06 11:19:13 +0530

    [Kritika] Added code for finding longest word in a sentence.

---
+++ core-java-questions/Java Strings/LongestWord.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LongestWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a sentence:");
+        String sentence = sc.nextLine();
+
+        String[] words = sentence.split(" ");
+        String longest = "";
+
+        for(int i = 0; i < words.length; i++) {
+            if(words[i].length() > longest.length()) {
+                longest = words[i];
+            }
+        }
+
+        System.out.println("Longest word: " + longest);
+
+        sc.close();
+    }
+}
