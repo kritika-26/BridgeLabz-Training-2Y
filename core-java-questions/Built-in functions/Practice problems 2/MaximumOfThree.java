9f36d255bad1469b2d970527bad74a1c0cc28542
kritika-26 <kritika.saxena_cs.aiml24@gla.ac.in>
2025-09-07 14:14:07 +0530

    [Kritika] Added code for finding largest number

---
+++ core-java-questions/Built-in functions/Practice problems 2/MaximumOfThree.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MaximumOfThree {
+    // method to take input
+    public static int getInput(Scanner sc, String msg) {
+        System.out.print(msg);
+        return sc.nextInt();
+    }
+
+    // method to find maximum
+    public static int findMax(int a, int b, int c) {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = getInput(sc, "Enter first number: ");
+        int num2 = getInput(sc, "Enter second number: ");
+        int num3 = getInput(sc, "Enter third number: ");
+
+        int max = findMax(num1, num2, num3);
+        System.out.println("Maximum number is: " + max);
+
+        sc.close();
+    }
+}
