2e1256efc0f8176a31fc6d10c400b578cbf2c57c
kritika-26 <kritika.saxena_cs.aiml24@gla.ac.in>
2025-09-07 14:10:40 +0530

    [Kritika] Added code for Fibonacci

---
+++ core-java-questions/Built-in functions/Practice problems 2/FibonacciSequence.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FibonacciSequence {
+    // method to print fibonacci sequence
+    public static void printFibonacci(int terms) {
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Sequence: ");
+        for (int i = 1; i <= terms; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+
+        printFibonacci(n);
+
+        sc.close();
+    }
+}
